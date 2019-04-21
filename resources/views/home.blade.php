83168d357436250fe4a21fa1f2b013333289c75c
Ahsan Zaman <ahsan,web.ml@gmail.com>
2019-04-21 19:58:31 +0300

    secure_asset

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
     <meta name='csrf-token' content = "{{ csrf_token() }}">
     <title>LaraVue</title>
 </head>
@@ -15,6 +15,6 @@
         </v-app>
     </div>
 
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{secure_asset('js/app.js')}}"></script>
 </body>
 </html>
